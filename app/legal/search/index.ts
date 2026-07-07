44910be34fdbd5551ae714af3fe0c74fcd66757f
ShinSungRok <sungrok1727@gmail.com>
2026-07-07 13:22:58 +0900

    refactor(search): add opensearch barrel exports

--- app/legal/search/index.ts
+++ app/legal/search/index.ts
@@ -3,18 +3,20 @@ export type { SearchQuery } from "./SearchQuery";
 export type { SearchResult } from "./SearchResult";
 export { KeywordSearchEngine } from "./KeywordSearchEngine";
 export { toRetrievalResult } from "./SearchResultMapper";
-export type { OpenSearchConfig } from "./opensearch/OpenSearchConfig";
-export type { OpenSearchLegalDocument } from "./opensearch/OpenSearchLegalDocument";
-export { toOpenSearchLegalDocument } from "./opensearch/OpenSearchLegalDocumentMapper";
-export { OPEN_SEARCH_LEGAL_INDEX_MAPPING } from "./opensearch/OpenSearchLegalIndexMapping";
-export type { OpenSearchClient } from "./opensearch/OpenSearchClient";
-export { OpenSearchIndexManager } from "./opensearch/OpenSearchIndexManager";
-export { OpenSearchLegalDocumentIndexer } from "./opensearch/OpenSearchLegalDocumentIndexer";
-export { buildOpenSearchKeywordSearchBody } from "./opensearch/OpenSearchSearchBodyBuilder";
-export { OpenSearchSearchEngine } from "./opensearch/OpenSearchSearchEngine";
 export type {
+  OpenSearchConfig,
+  OpenSearchLegalDocument,
+  OpenSearchClient,
   OpenSearchHit,
   OpenSearchSearchResponse,
-} from "./opensearch/OpenSearchSearchResponse";
-export { FakeOpenSearchClient } from "./opensearch/FakeOpenSearchClient";
-export { toSearchResults } from "./opensearch/OpenSearchSearchResponseMapper";
+} from "./opensearch";
+export {
+  toOpenSearchLegalDocument,
+  OPEN_SEARCH_LEGAL_INDEX_MAPPING,
+  OpenSearchIndexManager,
+  OpenSearchLegalDocumentIndexer,
+  buildOpenSearchKeywordSearchBody,
+  OpenSearchSearchEngine,
+  FakeOpenSearchClient,
+  toSearchResults,
+} from "./opensearch";
---
+++ app/legal/search/opensearch/index.ts
@@ -0,0 +1,15 @@
+export type { OpenSearchConfig } from "./OpenSearchConfig";
+export type { OpenSearchLegalDocument } from "./OpenSearchLegalDocument";
+export { toOpenSearchLegalDocument } from "./OpenSearchLegalDocumentMapper";
+export { OPEN_SEARCH_LEGAL_INDEX_MAPPING } from "./OpenSearchLegalIndexMapping";
+export type { OpenSearchClient } from "./OpenSearchClient";
+export { OpenSearchIndexManager } from "./OpenSearchIndexManager";
+export { OpenSearchLegalDocumentIndexer } from "./OpenSearchLegalDocumentIndexer";
+export { buildOpenSearchKeywordSearchBody } from "./OpenSearchSearchBodyBuilder";
+export { OpenSearchSearchEngine } from "./OpenSearchSearchEngine";
+export type {
+  OpenSearchHit,
+  OpenSearchSearchResponse,
+} from "./OpenSearchSearchResponse";
+export { FakeOpenSearchClient } from "./FakeOpenSearchClient";
+export { toSearchResults } from "./OpenSearchSearchResponseMapper";
